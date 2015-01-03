ebe548c47902b1644335e2359126c39c869bca7b
Rex Yuan <chihchengyuan@gmail.com>
2015-01-03 17:26:47 +0800

    update version info

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
     <div class="container-fluid" id="foot">
             <div class="row">
                 <div class="col-xs-12">
-                    <h6 class="text-center">courseNTNU version 00.00.00, by Yuan</h6>
+                    <h6 class="text-center">courseNTNU version 0.0, by Yuan</h6>
                 </div>
             </div>
         </div>
