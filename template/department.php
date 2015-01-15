43c2c6df3350b77cf2e238e639aef53b7266997e
Rex Yuan <chihchengyuan@gmail.com>
2015-01-15 19:17:34 +0800

    fixing url

--- template/department.php
+++ template/department.php
@@ -34,84 +34,84 @@
 </div>
 
 <div class="container" id="middle">
-    <form action="http://localhost/coursentnu/index.php" method="get" role="form">
+    <form action="http://localhost/index.php" method="get" role="form">
         
         <h3 id="CGE">通識</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=01U">藝術與美感</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=02U">哲學思維與道德推理</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=03U">公民素養與社會探究</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=04U">歷史與文化</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=05U">數學與科學思維</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=06U">科學與生命</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=01U">藝術與美感</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=02U">哲學思維與道德推理</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=03U">公民素養與社會探究</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=04U">歷史與文化</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=05U">數學與科學思維</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=06U">科學與生命</a></h4></li>
         </ul>
         
         <h3 id="COT">科技學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=IEU">工業教育學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=ITU">科技應用與人力資源發展學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=GCU">圖文傳播學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=MTU">機電科技學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=AEU">電機工程學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=IEU">工業教育學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=ITU">科技應用與人力資源發展學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=GCU">圖文傳播學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=MTU">機電科技學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=AEU">電機工程學系</a></h4></li>
         </ul>
         
         <h3 id="COE">教育學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=EDU">教育學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=PCU">教育心理與輔導學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=SOU">社會教育學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=HEU">健康促進與衛生教育學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=HGU">人類發展與家庭學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=CEU">公民教育與活動領導學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=SPU">特殊教育學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=EDU">教育學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=PCU">教育心理與輔導學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=SOU">社會教育學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=HEU">健康促進與衛生教育學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=HGU">人類發展與家庭學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=CEU">公民教育與活動領導學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=SPU">特殊教育學系</a></h4></li>
         </ul>
         
         <h3 id="CLA">文學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=CHU">國文學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=ENU">英語學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=HIU">歷史學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=GEU">地理學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=TCU">臺灣語文學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=CHU">國文學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=ENU">英語學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=HIU">歷史學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=GEU">地理學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=TCU">臺灣語文學系</a></h4></li>
         </ul>
         
         <h3 id="COS">理學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=MAU">數學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=PHU">物理學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=CMC">化學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=BIU">生命科學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=ESU">地球科學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=CSU">資訊工程學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=MAU">數學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=PHU">物理學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=CMC">化學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=BIU">生命科學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=ESU">地球科學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=CSU">資訊工程學系</a></h4></li>
         </ul>
         
         <h3 id="COM">音樂學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=MUU">音樂學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=MUU">音樂學系</a></h4></li>
         </ul>
         
         <h3 id="COB">管理學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=BAU">企業管理學士學位學程</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=BAU">企業管理學士學位學程</a></h4></li>
         </ul>
         
         <h3 id="COA">藝術學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=ATU">美術學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=VDU">設計學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=ATU">美術學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=VDU">設計學系</a></h4></li>
         </ul>
         
         <h3 id="CIS">國際與僑教學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=EAU">東亞學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=CLU">華語文教學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=TSU">應用華語文學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=EAU">東亞學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=CLU">華語文教學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=TSU">應用華語文學系</a></h4></li>
         </ul>
         
         <h3 id="CSR">運動與休閒學院</h3>
         <ul class="list-group">
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=PEU">體育學系</a></h4></li>
-            <li class="list-group-item"><h4><a href="<?= $urlroot ?>coursentnu/index.php?dpm=FPU">運動競技學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=PEU">體育學系</a></h4></li>
+            <li class="list-group-item"><h4><a href="<?= $urlroot ?>index.php?dpm=FPU">運動競技學系</a></h4></li>
         </ul>
     
     </form>
--- template/header.php
+++ template/header.php
@@ -5,13 +5,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     
-        <link href="<?= $urlroot ?>coursentnu/bootstrap.min.css" rel="stylesheet"/>
-        <link href="<?= $urlroot ?>coursentnu/bootstrap-theme.min.css" rel="stylesheet"/>
-        <link href="<?= $urlroot ?>coursentnu/styles.css" rel="stylesheet"/>
+        <link href="<?= $urlroot ?>bootstrap.min.css" rel="stylesheet"/>
+        <link href="<?= $urlroot ?>bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="<?= $urlroot ?>styles.css" rel="stylesheet"/>
         
-        <script src="<?= $urlroot ?>coursentnu/jquery-1.10.2.min.js"></script>
-        <script src="<?= $urlroot ?>coursentnu/bootstrap.min.js"></script>
-        <script src="<?= $urlroot ?>coursentnu/scripts.js"></script>
+        <script src="<?= $urlroot ?>jquery-1.10.2.min.js"></script>
+        <script src="<?= $urlroot ?>bootstrap.min.js"></script>
+        <script src="<?= $urlroot ?>scripts.js"></script>
         
         <?php if (isset($title)): ?>
             <title>courseNTNU: <?= htmlspecialchars($title) ?></title>
@@ -24,7 +24,7 @@
         <div class="container-fluid" id="head">
             <div class="row">
                 <div class="col-xs-12">
-                    <h1 class="text-center"><a href="<?= $urlroot ?>coursentnu/index.php">courseNTNU</a></h1>
+                    <h1 class="text-center"><a href="<?= $urlroot ?>index.php">courseNTNU</a></h1>
                 </div>
             </div>
         </div>
