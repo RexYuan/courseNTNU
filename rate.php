61e61b365affbff5b35f13e812e991783d5c2a15
Rex Yuan <chihchengyuan@gmail.com>
2015-01-18 19:33:56 +0800

    working fb login

--- rate.php
+++ rate.php
@@ -70,14 +70,15 @@
             $like_bar = "0%";
             $dislike_bar = "0%";
             $ratings = "N/A";
+            $message = "沒有資料";
         }
         else
         {
             $like_bar = ((string) ($like_percentage * 100)) . "%";
             $dislike_bar = ((string) ((1 - $like_percentage) * 100)) . "%";
             $ratings = sprintf('%2d', ($like_percentage * 100));
+            $message = "根據 $total 個投票";
         }
-        $message = "根據 $total 個投票";
 
         // echo JSON
         echo json_encode(["ratings" => $ratings, "like_bar" => $like_bar, "dislike_bar" => $dislike_bar, "message" => $message]);
