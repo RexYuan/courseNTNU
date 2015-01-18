3cdb2f2915f48323e66e4bce8b7158b0dbe765d0
Rex Yuan <chihchengyuan@gmail.com>
2015-01-18 23:04:22 +0800

    working fb login

--- btn.php
+++ btn.php
@@ -11,28 +11,8 @@
         $voterow = query("SELECT * FROM vote WHERE code = ? AND fbid = ?", $_POST["code"], $_POST["fbid"]);
         $vote = $voterow[0];
 
-        // if not yet voted
-        if (empty($voterow))
-        {
-            $result = 'no';
-        }
-        // voted
-        else
-        {
-            // liked it
-            if ($vote["vote"] == 1)
-            {
-                $result = 'likeit';
-            }
-            // disliked it
-            else if ($vote["vote"] == 0)
-            {
-                $result = "dislikeit";
-            }
-        }
-
         // echo JSON
-        echo json_encode(["vote" => $vote]);
+        echo json_encode(["vote" => $vote["vote"]]);
     }
     else
     {
