d9fc4b20bade2fd9185ce098c0ef5f80a63f1466
Rex Yuan <chihchengyuan@gmail.com>
2015-01-18 18:26:36 +0800

    working fb login

--- rate.php
+++ rate.php
@@ -71,7 +71,7 @@
         $ratings = sprintf('%2d', ($like_percentage * 100));
 
         // echo JSON
-        echo json_encode(["ratings" => $ratings, "like_bar" => $like_bar, "dislike_bar" => $dislike_bar, "message" => $message, "voted" => $good]);
+        echo json_encode(["ratings" => $ratings, "like_bar" => $like_bar, "dislike_bar" => $dislike_bar, "message" => $message]);
     }
     else
     {
