2b8ab81a5fd838031650274e62394694b0fb6736
Rex Yuan <chihchengyuan@gmail.com>
2015-01-18 20:55:46 +0800

    working fb login

--- btn.js
+++ btn.js
@@ -1,25 +1,25 @@
 $(function() {
+    console.log(cod);
+    console.log(fbID);
+    console.log(fbstatus);
     var cod = $('#code').val();
     if (fbstatus == "connected")
-        {
-$.post("btn.php", {"code": cod, "fbid": fbID})
-    .done(function( data ) {
-        // update html
-        console.log(cod);
-        console.log(fbID);
-        var datum = jQuery.parseJSON(data);
-        console.log(datum);
-        if (datum["vote"] == 'likeit')
-        {
-            $("#likebtn").addClass('active');
-        }
-        else if (datum["vote"] == 'dislikeit')
-        {
-            $("#dislikebtn").addClass('active');
-        }
-    })
-    .fail(function() {
-        alert("Error!Error!Error!這你也有問題？!");
-    });}
-    
+    {
+        $.post("btn.php", {"code": cod, "fbid": fbID})
+            .done(function( data ) {
+            // update html
+                var datum = jQuery.parseJSON(data);
+                console.log(datum);
+                if (datum["vote"] == 'likeit')
+                {
+                    $("#likebtn").addClass('active');
+                }
+                else if (datum["vote"] == 'dislikeit')
+                {
+                    $("#dislikebtn").addClass('active');
+                }
+            })
+            .fail(function() {
+                alert("Error!Error!Error!這你也有問題？!");
+            });}
     });
