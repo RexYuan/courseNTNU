d69fa81f0b5af7e722c68d68d8ef07e0b99b12a7
Rex Yuan <chihchengyuan@gmail.com>
2015-01-18 21:27:19 +0800

    working fb login

--- rate.php
+++ rate.php
@@ -28,7 +28,7 @@
                 }
                 else
                 {
-                    query("UPDATE vote SET vote = 0 WHERE fbid = ? AND code = ? AND vote = ?", $_POST["fbid"], $_POST["code"], '1');
+                    query("UPDATE vote SET vote = 1 WHERE fbid = ? AND code = ? AND vote = ?", $_POST["fbid"], $_POST["code"], '0');
                     query("UPDATE course SET likeit = likeit + 1 WHERE code = ?", $_POST["code"]);
                     query("UPDATE course SET dislikeit = dislikeit - 1 WHERE code = ?", $_POST["code"]);
                 }
@@ -53,7 +53,7 @@
                 }
                 else
                 {
-                    query("UPDATE vote SET vote = 0 WHERE fbid = ? AND code = ? AND vote = ?", $_POST["fbid"], $_POST["code"], '0');
+                    query("UPDATE vote SET vote = 0 WHERE fbid = ? AND code = ? AND vote = ?", $_POST["fbid"], $_POST["code"], '1');
                     query("UPDATE course SET dislikeit = dislikeit + 1 WHERE code = ?", $_POST["code"]);
                     query("UPDATE course SET likeit = likeit - 1 WHERE code = ?", $_POST["code"]);
                 }
