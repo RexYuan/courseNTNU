c1130114590b7d9b55150e71d34d6591f2251fcf
Rex Yuan <chihchengyuan@gmail.com>
2015-01-22 15:19:48 +0800

    REALLY! version 0.0 done finally!

--- report_clean_0.0.sql
+++ report_clean_0.0.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:7777
--- Generation Time: Jan 22, 2015 at 02:23 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Host: localhost
+-- Generation Time: Jan 22, 2015 at 03:18 PM
+-- Server version: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `coursentnu`
@@ -20,31 +26,16 @@ SET time_zone = "+00:00";
 -- Table structure for table `report`
 --
 
-CREATE TABLE `report` (
-`id` int(20) NOT NULL,
-  `report` text CHARACTER SET latin1 NOT NULL,
+CREATE TABLE IF NOT EXISTS `report` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `report` text COLLATE utf8_unicode_ci NOT NULL,
   `fbID` bigint(30) NOT NULL,
-  `fbMail` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `fbName` varchar(10) CHARACTER SET latin1 NOT NULL,
-  `fbGender` varchar(10) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `fbMail` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `fbName` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `fbGender` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=14 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `report`
---
-ALTER TABLE `report`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `report`
---
-ALTER TABLE `report`
-MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- vote_clean_0.0.sql
+++ vote_clean_0.0.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:7777
--- Generation Time: Jan 22, 2015 at 02:24 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Host: localhost
+-- Generation Time: Jan 22, 2015 at 03:17 PM
+-- Server version: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `coursentnu`
@@ -20,31 +26,17 @@ SET time_zone = "+00:00";
 -- Table structure for table `vote`
 --
 
-CREATE TABLE `vote` (
-`id` int(30) NOT NULL,
+CREATE TABLE IF NOT EXISTS `vote` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `fbid` bigint(30) unsigned NOT NULL,
-  `code` varchar(10) CHARACTER SET latin1 NOT NULL,
+  `code` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `vote` tinyint(1) NOT NULL COMMENT '1 for yay 0 for nay',
-  `fbName` varchar(10) CHARACTER SET latin1 NOT NULL,
-  `fbGender` varchar(10) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `fbName` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fbGender` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fbid` (`fbid`,`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `vote`
---
-ALTER TABLE `vote`
- ADD PRIMARY KEY (`id`), ADD KEY `fbid` (`fbid`,`code`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `vote`
---
-ALTER TABLE `vote`
-MODIFY `id` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
