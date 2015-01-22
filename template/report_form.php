270a3bbf1467b2f638fe4f211ae9325badd4e5f1
Rex Yuan <chihchengyuan@gmail.com>
2015-01-23 02:35:57 +0800

    updated report message

--- template/report_form.php
+++ template/report_form.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <label class="col-xs-12 control-label text-center" for="report_text">
                     <h3>回報問題或提供意見</h3>
-                    <p>courseNTNU目前還是Beta版，有很大的可能性會出錯或是有課程資訊有誤，如果發現問題，請回報給開發人員，謝謝！</p>
+                    <p>courseNTNU目前還是Beta版，有很大的可能性會出錯或是有課程資訊有誤，如果發現問題，請回報給開發人員，謝謝！如果你想要參與或作出貢獻的，也請利用本表格告訴我們！</p>
                 </label>
             </div>
             <div class="alert alert-success hidden" id="report_success_message" role="alert">成功送出！</div>
