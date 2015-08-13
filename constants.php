4a4ef085a6e45aa05cbed20bfd1da3c92ed58e6d
RexYuan <r1218r1218@gmail.com>
2015-08-13 20:20:57 +0800

    changed url base

--- constants.php
+++ constants.php
@@ -1,6 +1,7 @@
 <?php
 
-    $urlroot = "http://coursentnu.com/";
+    //$urlroot = "http://coursentnu.com/";
 	// $urlroot = "http://localhost/";
+  $urlroot = "http://".$_SERVER['SERVER_NAME']."/";
 
 ?>
