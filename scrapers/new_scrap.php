6c5153f11ca7e4890a86f9c0071ae727de098aa3
RexYuan <r1218r1218@gmail.com>
2015-08-21 04:01:21 +0800

    working on scraper
    to be continued

--- scrapers/new_scrap.php
+++ scrapers/new_scrap.php
@@ -2,6 +2,27 @@
   // requirements
   require("new_scrap_helper.php");
 
+  // departments
+  foreach ($DEPARTMENT_CODE_LIST as $dept_code => $dept_name)
+  {
+    // checking if already in database
+    $result = query("SELECT ChName FROM Departments WHERE DeptCode = ?", $dept_code);
+    if ($result)
+    {
+      // if name was changed
+      if ($result[0]['ChName'] != $dept_name)
+      {
+        query("UPDATE Departments SET ChName = ? WHERE DeptCode = ?", $dept_name, $dept_code);
+      }
+    }
+    // new dept
+    else
+    {
+      query("INSERT INTO Departments (DeptCode, ChName) VALUES (?, ?)", $dept_code, $dept_name);
+    }
+  }
+
+  // courses
   foreach (array_keys($DEPARTMENT_CODE_LIST) as $dept_code)
   {
     // build URL
@@ -11,33 +32,52 @@
     $raw_json = file_get_contents($target);
     $json = json_decode($raw_json, $assoc = True);
     // retrieve information (note that every value in JSON is string)
-    $number_of_course                = $json['Count'];          // 課程數目
+    $number_of_course = $json['Count']; // 課程數目
     foreach ($json['List'] as $course)
     {
-      $course_chinese_name           = $course['chn_name'];        // 課程中文名稱
-      $course_english_name           = $course['eng_name'];        // 課程英文名稱
-      $course_teacher                = $course['teacher'];         // 教師
-      $course_code                   = $course['course_code'];     // 課程代碼
-      $course_serial                 = $course['serial_no'];       // 開課序號
-      $course_time_location          = $course['time_inf'];        // 時間與開課地點
-      $course_group                  = $course['course_group'];    // 課程組
-      $course_length                 = $course['course_kind'];     // 全／半年
-      $course_credit                 = $course['credit'];          // 學分數
-      $course_elective               = $course['option_code'];     // 必／選修
-      $course_grade                  = $course['form_s'];          // 開課年級
-      $course_grade_chinese_name     = $course['form_s_name'];     // 開課年級中文名稱
-      $course_is_master              = $course['class_name'];      // 大碩合開
-      $course_is_in_english          = $course['eng_teach'];       // 全英語授課
-      $course_restrict               = $course['restrict'];        // 限制
-      $course_gender                 = $course['gender_restrict']; // 性別限制
-      $course_comment                = $course['comment'];         // 註解
-      $department_chinese_name       = $course['dept_chiabbr'];    // 系所中文名稱
-      $department_code               = $course['dept_code'];       // 系所代碼
-      $department_group              = $course['dept_group'];      // 系所組別
-      $department_group_chinese_name = $course['dept_group_name']; // 系所組別中文名稱
+      // 簡單資訊
+      $SerialNo = (int)$course['serial_no']; // 開課序號
+      $CourseCode = (string)$course['course_code']; // 課程代碼
+      $AcadmYear = (int)$YEAR; // 學年
+      $AcadmTerm = (int)$TERM; // 學期
+      $ChName = (string)$course['chn_name']; // 課程中文名稱
+      $EnName = (string)$course['eng_name']; // 課程英文名稱
+      $CourseGroup = (string)$course['course_group']; // 組別
+      $ClassCode = (string)$course['classes']; // 開課班級代碼
+      $Credit = (float)$course['credit']; // 學分數
+      $DeptGroup = (string)$course['dept_group']; // 開課組別
+      $Grade = (string)$course['form_s']; // 開課年級
+      $RestrictInfo = (string)$course['restrict']; // 限制
+      $selfTeachName = (string)$course['selfTeachName']; // *正課/實驗親授
+      $EnLocation = Null; // 英文上課地點
+      $StatusInfo = False; // *是否停開
+      $ChComment = (string)$course['comment']; // 中文註解
+      $EnComment = Null; // 英文註解
+      $CourseSize = (int)$course['counter_exceptAuth']; // 修課總人數
+      $AuthMaxSize = (int)$course['authorize_p']; // 授權碼名額
+      $AuthRate = (float)$course['authorize_r']; // 授權碼比例
+      $AuthUsed = (int)$course['authorize_using']; // 授權碼使用人數
+      $NTAMaxSize = (int)$course['limit']; // 台大聯盟限修人數
+      $TotalMaxSize = (int)$course['limit_count_h']; // 限修人數
+      // 簡單處理資訊
+      $Duration = $course['course_kind'] == "半" ? "H" : "F"; // 全/半學期
+      $IsEngTeach = $course['eng_teach'] == "是" ? True : False; // 全英語授課
+      $GenderRestrict = $course['gender_restrict'] == "" ? "N" : $course['gender_restrict']; // 性別限制
+      $IsMOOC = $course['moocs_teach'] == "是" ? True : False; // MOOCs
+      $IsElective = $course['option_code'] == "選" ? True : False; // 必／選修
+      $RemoteTeach = $course['rt'] == "是" ? True : False; // 遠距授課
+      // 判斷資訊
+      $DeptId = lookup_dept_id_by_code($course['dept_code']); // 開課系所識別碼
+      $TeacherId = lookup_teacher_id_by_name($DeptId, $course['teacher']); // 教師
+      list($ChLocation, $TimeInfo) = array_values(parse_time_inf($course['time_inf'])); // 中文上課地點, 上課時間
+      // 課綱資訊
+      list($Hour, $Description) = lookup_course_page($course['course_code'], $course['course_group'], $course['dept_code'], $course['form_s'], $course['classes'], $course['dept_group']);
+      // 選課系統資訊
+      $FreshReserve = Null;
+      $Distributed = Null;
+      //TODO:1. test all values 2. store them up 3. deal with records
       // store data
-      // FILL ME UP!
-      print_r([$number_of_course,$course_english_name,$course_grade,$course_serial]);;
+      print_r([$number_of_course,$EnName,(int)$IsElective,$TeacherId,$ChLocation,$Hour]);;
     }
   }
  ?>
--- scrapers/new_scrap_helper.php
+++ scrapers/new_scrap_helper.php
@@ -1,49 +1,44 @@
 <?php
-
-/**
- * Executes SQL statement, possibly with parameters, returning
- * an array of all rows in result set or FALSE on (non-fatal) error.
- */
  require("../magic.php");
- function query($sql, ...$parameters)
+ /* For more info on this, read functions.php
+  * The return value of this function is
+  * an array containg all the retrieved rows
+  * as associative arrays with column => value
+  * structure.
+  */
+ function query()
  {
-  // try to connect to database
+  $sql = func_get_arg(0);
+  $parameters = array_slice(func_get_args(), 1);
   static $handle;
   if (!isset($handle))
   {
     try
     {
-      // connect to database
-      $handle = new PDO("mysql:dbname=coursentnu;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;port=3306", USERNAME, PASSWORD);
+      $handle = new PDO("mysql:dbname=testing;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;port=3306", USERNAME, PASSWORD);
       //$handle = new PDO("mysql:dbname=coursentnu;host=localhost;port=3306", USERNAME, PASSWORD);
-      // ensure that PDO::prepare returns FALSE when passed invalid SQL
-      $handle->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+      $handle->setAttribute(PDO::ATTR_EMULATE_PREPARES, False);
     }
     catch (Exception $e)
     {
-      // trigger (big, orange) error
       trigger_error($e->getMessage(), E_USER_ERROR);
-       exit;
+      exit;
     }
   }
-  // prepare SQL statement
   $statement = $handle->prepare($sql);
-  if ($statement === FALSE)
+  if ($statement === False)
   {
-    // trigger (big, orange) error
     trigger_error($handle->errorInfo()[2], E_USER_ERROR);
     exit;
   }
-  // execute SQL statement
   $results = $statement->execute($parameters);
-  // return result set's rows, if any
-  if ($results !== FALSE)
+  if ($results !== False)
   {
     return $statement->fetchAll(PDO::FETCH_ASSOC);
   }
   else
   {
-     return FALSE;
+     return False;
   }
  }
 
@@ -55,7 +50,6 @@ $DEPT_URL_GET_BASE = "http://courseap.itc.ntnu.edu.tw/acadmOpenCourse/CofnameCtr
 $DEPARTMENT_CODE_LIST = parse_dept_code(file_get_contents($DEPT_URL_GET_BASE . "type=" . $DEPT_LANGUAGE . "&year=" . $YEAR ."&term=" . $TERM));
 
 $URL_GET_BASE = 'http://courseap.itc.ntnu.edu.tw/acadmOpenCourse/CofopdlCtrl?';
-
 $URL_GET_LIST = [
     '_dc'          => '',
     'acadmYear'    => $YEAR,
@@ -104,15 +98,19 @@ function parse_time_inf($str)
     if (!empty($str))
     {
         // to be returned
-        $result = [];
+        //$result = [];
+        //temporarily changing this output format because course simulation is not yet ready
+        $result = ["ChLocation"=>"","TimeInfo"=>""];
         // check if there's more than one time
         $sub_strs = array_slice(explode(",", $str), 0, $length = -1);
         // parse every time
         foreach ($sub_strs as $sub_str)
         {
-
             list($day, $time, $campus, $location) = explode(" ", $sub_str);
-            $result[] = [$day, $time, $campus, $location];
+            //$result[] = [$day, $time, $campus, $location];
+            //temporarily changing this output format because course simulation is not yet ready
+            $result["ChLocation"] = $result["ChLocation"].$day.$time;
+            $result["TimeInfo"] = $result["TimeInfo"].$campus.$location;
         }
         // return an array of arrays of info
         return $result;
@@ -128,7 +126,7 @@ function parse_dept_code($dept_str) {
     $dept_str = substr($dept_str, 1, strlen($dept_str) - 2);
     $tok = strtok($dept_str , "]");
 
-    while($tok !== FALSE) {
+    while($tok !== False) {
       $tok = substr($tok, strpos($tok, "[") + 1);
       sscanf($tok, "'%*[^']','%[^ ]%[^'']'", $code, $name);
       $dept_list[$code] = $name;
@@ -136,5 +134,65 @@ function parse_dept_code($dept_str) {
     }
 
     return $dept_list;
+}
+
+ /**
+  * This function takes a teacher's Chinese name(string)
+  * and the teacher's department. Then, try to look up teacher's id.
+  * If the name is not yet recorded in Teacheres table,
+  * insert its info and returns the id.
+  * If the name is already recorded, returns id right away.
+  */
+function lookup_teacher_id_by_name($dept_id, $name)
+{
+  $result = query("SELECT TeacherId FROM Teachers WHERE ChName = ? AND DeptId = ?", $name, $dept_id);
+  //TODO: solve the problem of multiple teachers with identical name in the same department
+  if ($result)
+  {
+    return $result[0]['TeacherId'];
+  }
+  else
+  {
+    query("INSERT INTO Teachers (DeptId, ChName) VALUES (?, ?)", $dept_id, $name);
+    return query("SELECT TeacherId FROM Teachers WHERE ChName = ? AND DeptId = ?", $name, $dept_id)[0]['TeacherId'];
+  }
+}
+
+/**
+ * This function takes a DeptCode and returns its DeptId.
+ */
+function lookup_dept_id_by_code($dept_code)
+{
+  return query("SELECT DeptId FROM Departments WHERE DeptCode = ?", $dept_code)[0]['DeptId'];
+}
+
+/**
+ * This function is used to parse the hour string
+ * in course page and returns the hour value as an int.
+ * Example: $str      = "正課時數 : 2 小時"
+ *          $match[0] = 2
+ */
+function parse_hour($str)
+{
+  preg_match("/\d/",$str,$match);
+  return (int)$match[0];
+}
+
+/**
+ * This function takes required get request variable to
+ * retrieve infomation from course page.
+ * This function returns [$hour, $description].
+ * For more details, see scraper_helper_functions.php.
+ */
+function lookup_course_page($courseCode, $courseGroup, $deptCode, $formS, $classes1, $deptGroup)
+{
+  global $YEAR, $TERM;
+  $tidy =  new tidy;
+  $tidy -> parseFile("http://courseap.itc.ntnu.edu.tw/acadmOpenCourse/SyllabusCtrl?year=$YEAR&term=$TERM&courseCode=$courseCode&courseGroup=$courseGroup&deptCode=$deptCode&formS=$formS&classes1=$classes1&deptGroup=$deptGroup", ["output-xhtml"=>true,"indent"=>true,"indent-attributes"=>true,"numeric-entities"=>true,"bare"=>true,"clean"=>true,"word-2000"=>true,"wrap"=>0], "utf8");
+  $tidy -> cleanRepair();
+  $xhtml = preg_replace('/\n\s*<tr>\n\s*<td\swidth="900"\n\s*align="left"\n\s*bgcolor="#DFEFFF"\n\s*colspan="3"\n\s*height="20">\n\s*<b>教學進度與主題<\/b>\n\s*<\/td>\n\s*<\/tr>.*\n\s*<\/td>\n\s*<\/tr>/s', '', (string)$tidy);
+  $dom = new SimpleXMLElement($xhtml);
+  $dom->registerXPathNamespace("xhtml", "http://www.w3.org/1999/xhtml");
+  return [parse_hour(trim((string)$dom->xpath("/xhtml:html/xhtml:body/xhtml:div/xhtml:table[4]/xhtml:tr[4]/xhtml:td[4]")[0])), trim((string)$dom->xpath("/xhtml:html/xhtml:body/xhtml:div/xhtml:table[4]/xhtml:tr[7]/xhtml:td[2]")[0])];
 }
 ?>
